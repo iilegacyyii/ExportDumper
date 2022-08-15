942375d364ab1ce10b5c5e2b28b3dccc5d674969
iilegacyyii <68760809+iilegacyyii@users.noreply.github.com>
2022-08-15 22:29:26 +0100

    Initial Commit (again)

---
+++ ExportDump/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExportDump
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ensure user has specified arguments.
+            if (args.Length != 1)
+            {
+                Console.WriteLine(@"[!] Must specify filename argument.");
+                Console.WriteLine(@"Usage: .\ExportDump.exe C:\Windows\System32\version.dll");
+                return;
+            }
+
+            // Attempt to open user-specified DLL.
+            string filepath = args[0];
+            if (!File.Exists(filepath))
+            {
+                Console.WriteLine($"[!] Could not open '{filepath}'. Ensure you have permissions to read the specified file.");
+                return;
+            }
+
+
+            // Dump exported functions
+            string exports = "";
+            PeNet.PeFile pe = new PeNet.PeFile(filepath);
+            Console.WriteLine($"[*] Dumping all {pe.ExportedFunctions.Length} exported functions from {filepath}.");
+            foreach (PeNet.Header.Pe.ExportFunction exportFunction in pe.ExportedFunctions)
+            {
+                exports += String.Format("#pragma comment(linker, \"/export:{0}={1}.{0},@{2}\")\n",
+                    exportFunction.Name, 
+                    Path.GetDirectoryName(filepath).Replace("\\", "\\\\") + "\\\\" + Path.GetFileNameWithoutExtension(filepath), 
+                    exportFunction.Ordinal
+                );
+            }
+            
+            Console.WriteLine($"[+] Dumping done. Check pragma.h");
+            File.WriteAllText("pragma.h", exports);
+        }
+    }
+}
---
+++ ExportDump/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ExportDump")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExportDump")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6a5fdb37-e0b9-4e56-b5f9-dabe72441bfa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can dexportFunctionault the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
